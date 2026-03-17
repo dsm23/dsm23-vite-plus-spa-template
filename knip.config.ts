0af654c4d43f9e8f583feef7b4c75cf3fa134c3b
David Murdoch <dsmurdoch357@gmail.com>
2026-03-17 14:06:53 +0000

    chore(lint): add commitlint

--- knip.config.ts
+++ knip.config.ts
@@ -3,6 +3,7 @@ import type { KnipConfig } from "knip";
 const config: KnipConfig = {
   tags: ["-lintignore"],
   // entry: ["src/**/*.d.ts"],
+  ignoreDependencies: ["@commitlint/cli"],
 };
 
 export default config;
