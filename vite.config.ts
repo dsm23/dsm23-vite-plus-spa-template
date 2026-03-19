6ff60a90f79bd8973c0a2af2778aadd1cc374cac
David Murdoch <dsmurdoch357@gmail.com>
2026-03-19 11:07:49 +0000

    chore(tailwind): add tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite-plus";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
@@ -118,5 +119,5 @@ export default defineConfig({
       "vitest/no-importing-vitest-globals": "warn",
     },
   },
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
 });
